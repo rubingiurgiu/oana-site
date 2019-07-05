29bf2ce76193a2cb4f37d0cce1fcee8ff8b95cc3
Rubin Giurgiur <R.Giurgiu@plan-net.com>
2019-07-05 11:05:00 +0300

    home page home controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function homeAction(Request $request)
+    {
+        return $this->render('home.html.twig', []);
+    }
+}
